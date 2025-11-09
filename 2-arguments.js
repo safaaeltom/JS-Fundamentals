d51c2a313cb287a3cf7d411bb3c043833ff99a2a
safaaeltom <safaaeltom@gmail.com>
2025-11-09 18:41:13 +0300

    added arguments file to repo

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+let age = 20;
+if (age >= 18) {
+    console.log("No argument");
+} else if (age >= 15) {
+    console.log("Argument found");
+} else (age < 15) {
+    console.log("Arguments found");
+}
