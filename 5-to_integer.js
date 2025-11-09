5767a5f66193e40847e2800345787b30f4c94308
safaaeltom <safaaeltom@gmail.com>
2025-11-09 21:20:23 +0300

    added integer file to repo

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const [, , arg] = process.argv;
 const num = Number(arg);
 
 if (!isNaN(num)) {
-  console.log(My number: ${num});
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
